f75dccfbdea9c8ec73647f13f3e1431610d03558
Agaile Victor <avictor@silversky.com>
2020-04-12 12:23:47 +0800

    added file upload component starter template

--- web-app/src/pages/jobs/jobs.ts
+++ web-app/src/pages/jobs/jobs.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'page-jobs',
   templateUrl: 'jobs.html',
 })
 export class JobsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public fileName: string = '';
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JobsPage');
   }
 
-  test() {
-    alert('clicked');
+  onBrowseClick($e) {
+    var self = this;
+    let loading = self.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    loading.present();
+    $e.preventDefault();
+    $('#jd').click();
+    loading.dismiss();
+  }
+
+  onFileSelected() {
+    var self = this;
+    var d = $("#jd").val().toString().split('\\');
+    self.fileName = d[d.length - 1];
   }
 }
